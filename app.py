81ad51e24c6d12887b7c9e5bfaeffa339131b6db
Rapeepath <106863267+Rapeepath@users.noreply.github.com>
2025-01-24 14:02:29 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+class student():
+  std_count = 0
+
+  def _new_(self):
+    print("Student._new_")
+
+  def _init_(self):
+    print("Student._init_)
+  
+  def study_hard(self):
+    print("sir,yes sir")
+  
